6539848d4ed58db204bdcaee4005bd3eb3d38a2c
jui <juiyezarkar1999@gmail.com>
2026-08-03 17:07:09 +0530

    class componet example

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import FunctionalComponent from "./functionalComponent/page";
+import Welcome from "./ClassComponent/page";
+
 
 export default function Home() {
   return (
     <div>
       <FunctionalComponent />
+      <Welcome />
     </div>
   );
 }
