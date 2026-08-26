2cbddfb5eb3964302d6faa697962d1d683add176
jui <juiyezarkar1999@gmail.com>
2026-08-26 19:11:29 +0530

    added the traffic running

--- app/TrafficLight/page.tsx
+++ app/TrafficLight/page.tsx
@@ -1,33 +1,52 @@
 'use client'
-import { useEffect, useState } from "react";
-const TrafficLight=function(){
-    const [Color,setColor]=useState('');
-  let lights=['Red','yellow','Green'];
-  let index=0;
-  useEffect(()=>{
-    console.log("ist running")
-     let timer=setInterval(()=>{
-    setColor(lights[index]);
-    index++;
-      if(index===lights.length){
-        index=0;
-      }
-  },1000)
 
-    clearInterval(timer);
-  })
-  
+import { useEffect, useState, useRef } from "react";
 
+const TrafficLight = function () {
 
+    const [Color, setColor] = useState('');
 
+    let lights = ['Red', 'yellow', 'Green'];
 
-  return(
-    <>
-    <div style={{color:Color,width:'5px',height:'5px'}}></div>
-    </>
-  )
+    let index = useRef(0);
+
+    useEffect(() => {
+
+        console.log("is running");
+
+        let timer = setInterval(() => {
+
+            console.log("lights", lights[index.current]);
+
+            setColor(lights[index.current]);
+
+            index.current++;
+
+            if (index.current === lights.length) {
+                index.current = 0;
+            }
+
+        }, 2000);
+
+        return () => {
+            clearInterval(timer);
+        };
+
+    }, []);
+
+    return (
+        <>
+            <div
+                style={{
+                    color: Color,
+                    width: '50px',
+                    height: '50px'
+                }}
+            >
+                {Color}
+            </div>
+        </>
+    )
 }
-
-
 
 export default TrafficLight;
